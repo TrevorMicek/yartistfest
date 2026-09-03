04491244ed625d1cd06b6c697a39c7a8898fce15
Trevor Micek <webdevtrevor@gmail.com>
2026-09-02 22:05:51 -0600

    updated motion

--- src/components/FadeIn.jsx
+++ src/components/FadeIn.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { motion, useReducedMotion } from "framer-motion";
+import { motion } from "motion/react";
 
 const FadeInStaggerContext = createContext(false);
 
