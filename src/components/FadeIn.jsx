c74c06a51c906dcd4ee48f9f0c7296f54bd8177d
Trevor Micek <webdevtrevor@gmail.com>
2026-09-02 22:10:34 -0600

    fixed motion finally

--- src/components/FadeIn.jsx
+++ src/components/FadeIn.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { motion } from "motion/react";
+import { motion, useReducedMotion } from "motion/react";
 
 const FadeInStaggerContext = createContext(false);
 
